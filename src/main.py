55d60a15550591ecaa53837280b9f2c187ae3659
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-03-22 04:25:18 -0300

    chore: declares both audios folders

--- src/main.py
+++ src/main.py
@@ -2,17 +2,17 @@ import os
 import subprocess
 import wave
 
-AUDIOS_SEM_ROBO_DIR = os.path.join("..", "audios_tc_sem_robo")
-AUDIOS_CORTADOS_DIR = os.path.join("..", "audios_sem_robo_cortados")
+AUDIOS_WITHOUT_ROBOTS_DIR = os.path.join("..", "without_robots")
+AUDIOS_WITHOUT_ROBOTS_CUTTED_DIR = os.path.join("..", "without_robots_cutted")
 
 
 def get_most_shortest_audio() -> float:
     shortest_duration = float("inf")
     shortest_file = None
 
-    for filename in os.listdir(AUDIOS_SEM_ROBO_DIR):
+    for filename in os.listdir(AUDIOS_WITHOUT_ROBOTS_DIR):
         if filename.endswith(".wav"):
-            filepath = os.path.join(AUDIOS_SEM_ROBO_DIR, filename)
+            filepath = os.path.join(AUDIOS_WITHOUT_ROBOTS_DIR, filename)
 
             try:
                 with wave.open(filepath, "rb") as audio_file:
@@ -45,10 +45,10 @@ def format_seconds_to_ffmpeg_time_format(seconds: float) -> str:
 
 
 def cut_audios(to_time: str) -> None:
-    for root, _, files in os.walk(AUDIOS_SEM_ROBO_DIR):
+    for root, _, files in os.walk(AUDIOS_WITHOUT_ROBOTS_DIR):
         for filename in files:
             filepath = os.path.join(root, filename)
-            cutted_audio_path = os.path.join(AUDIOS_CORTADOS_DIR, filename)
+            cutted_audio_path = os.path.join(AUDIOS_WITHOUT_ROBOTS_CUTTED_DIR, filename)
 
             command = f"ffmpeg -i {filepath} -ss 00:00:00 -to {to_time} -c copy {cutted_audio_path}"
 
@@ -74,9 +74,9 @@ def get_audio_duration(filepath: str) -> float:
 def get_audios_average_duration() -> None:
     durations = []
 
-    for filename in os.listdir(AUDIOS_SEM_ROBO_DIR):
+    for filename in os.listdir(AUDIOS_WITHOUT_ROBOTS_DIR):
         if filename.endswith(".wav"):
-            filepath = os.path.join(AUDIOS_SEM_ROBO_DIR, filename)
+            filepath = os.path.join(AUDIOS_WITHOUT_ROBOTS_DIR, filename)
             duration = get_audio_duration(filepath)
 
             durations.append(duration)
